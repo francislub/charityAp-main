8c18774df3dbc72ffe4fe107721d76bdd6e6f87d
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-05-28 23:06:23 -0700

    nav

--- app/programs/page.jsx
+++ app/programs/page.jsx
@@ -18,7 +18,11 @@ import educationImage3 from '../public/images/programs/3.jpg';
 import bannerImage from '../public/images/banner.jpg'
 
 export default function ProgramsPage() {
-  
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
     return (
         <div className="scroll-smooth text-gray-800">
             <div className="fullContainer banner" id="homeSection">
@@ -32,54 +36,54 @@ export default function ProgramsPage() {
                                 className="rounded-md"/>
                         </div>
 
-                        <nav className="pt-5">
-                            <ul>
-                                <li class="dropdown">
-                                    <a href="/" className="dropbtn link">ABOUT</a>
-                                    <div class="dropdown-content">
-                                        <Link href="/about" className="link">WHO WE ARE</Link>
-                                        <Link href="/wedo" className="link">WHAT WE DO</Link>
-                                        <Link href="/impact" className="link">OUR IMPACT</Link>
-                                        <Link href="/team" className="link">OUR LEADERSHIP</Link>
-                                        <Link href="/joinus" className="link">JOIN OUR TEAM</Link>
-                                    </div>
-                                </li>
-                                <li class="dropdown">
-                                    <a href="#" className="dropbtn link">PROGRAMS</a>
-                                    <div class="dropdown-content">
-                                        <Link href="/education" className="link">EDUCATION</Link>
-                                        <Link href="/community" className="link">COMMUNITY ENGAGEMENT</Link>
-                                    </div>
-                                </li>
-                                {/* <li>
-                                    <Link href="/programs" className="link">
-                                        Programs
-                                    </Link>
-                                </li> */}
-                                <li class="dropdown">
-                                    <a href="#" className="dropbtn link">CHAPTERS</a>
-                                    <div class="dropdown-content">
-                                        <Link href="/denmark" className="link">DENMARK</Link>
-                                        <Link href="/switzerland" className="link">SWITZERLAND</Link>
-                                        <Link href="/germany" className="link">GERMANY</Link>
-                                    </div>
-                                </li>
-                                <li>
-                                    <Link href="/gallery" className="link">
-                                        Gallery
-                                    </Link>
-                                </li>
-                                {/* <li>
-                                    <Link href="/joinus" className="link">
-                                        Join Us
-                                    </Link>
-                                </li> */}
-                                <li>
-                                    <Link href="/donate" className="link bg-amber-700 rounded-lg">
-                                        Donate
-                                    </Link>
-                                </li>
-                            </ul>
+                        <nav className="pt-5 relative">
+                            <div className="flex items-center justify-between px-4 py-3 md:flex md:space-x-4">
+                                <div className="flex justify-between items-center w-full md:w-auto">
+                                {!isOpen && (
+                                    <button
+                                        className="md:hidden text-gray-900 focus:outline-none relative top-4 right-4 z-50"
+                                        onClick={() => setIsOpen(!isOpen)}
+                                    >
+                                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                                        </svg>
+                                    </button>
+                                )}
+                                </div>
+                                <ul className={`mt-4 md:mt-0 md:flex md:space-x-4 ${isOpen ? 'block' : 'hidden'}`}>
+                                    <li className="dropdown relative">
+                                        <a href="/" className="dropbtn link ">ABOUT</a>
+                                        <div className="dropdown-content absolute hidden bg-white shadow-lg rounded-md">
+                                            <Link href="/about" className="link block px-4 py-2 text-gray-700">WHO WE ARE</Link>
+                                            <Link href="/wedo" className="link block px-4 py-2 text-gray-700">WHAT WE DO</Link>
+                                            <Link href="/impact" className="link block px-4 py-2 text-gray-700">OUR IMPACT</Link>
+                                            <Link href="/team" className="link block px-4 py-2 text-gray-700">OUR LEADERSHIP</Link>
+                                            <Link href="/joinus" className="link block px-4 py-2 text-gray-700">JOIN OUR TEAM</Link>
+                                        </div>
+                                    </li>
+                                    <li className="dropdown relative">
+                                        <a href="#" className="dropbtn link">PROGRAMS</a>
+                                        <div className="dropdown-content absolute hidden bg-white shadow-lg rounded-md">
+                                            <Link href="/education" className="link block px-4 py-2 text-gray-700">EDUCATION</Link>
+                                            <Link href="/community" className="link block px-4 py-2 text-gray-700">COMMUNITY ENGAGEMENT</Link>
+                                        </div>
+                                    </li>
+                                    <li className="dropdown relative">
+                                        <a href="#" className="dropbtn link">CHAPTERS</a>
+                                        <div className="dropdown-content absolute hidden bg-white shadow-lg rounded-md">
+                                            <Link href="/denmark" className="link block px-4 py-2 text-gray-700">DENMARK</Link>
+                                            <Link href="/switzerland" className="link block px-4 py-2 text-gray-700">SWITZERLAND</Link>
+                                            <Link href="/germany" className="link block px-4 py-2 text-gray-700">GERMANY</Link>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <Link href="/gallery" className="link block px-4 py-2 text-gray-700">Gallery</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/donate" className="link bg-amber-700 rounded-lg block px-4 py-2 text-white">Donate</Link>
+                                    </li>
+                                </ul>
+                            </div>
                         </nav>
 
                     </div>
