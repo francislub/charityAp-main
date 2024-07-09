09ccbbb7de26690bf2f1907093bca3dea4d05547
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-07-08 20:03:40 -0700

    home

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -32,13 +32,17 @@ export default function HomePage() {
                             </div>
                             <div className="background-photo" style={{backgroundImage: "url('/logo1.png')"}}></div>
                         </div>
-                        <div className="logo">
-                            <Image 
+                        <Link href="/">
+                            <a className="logo">
+                                <Image 
                                 src={logoImage} 
                                 height={100} 
+                                width={100} // Add width to prevent layout shift
                                 alt="Foundation Logo" 
-                                className="rounded-md"/>
-                        </div>
+                                className="rounded-md"
+                                />
+                            </a>
+                            </Link>
 
                         <nav className="pt-5 relative">
                             <div className="flex items-center justify-between px-4 py-3 md:flex md:space-x-4">
@@ -56,7 +60,7 @@ export default function HomePage() {
                                 </div>
                                 <ul className={`mt-4 md:mt-0 md:flex md:space-x-4 ${isOpen ? 'block' : 'hidden'}`}>
                                     <li className="dropdown relative">
-                                        <a href="/" className="dropbtn link ">ABOUT</a>
+                                        <a href="#" className="dropbtn link ">ABOUT</a>
                                         <div className="dropdown-content absolute hidden bg-white shadow-lg rounded-md">
                                             <Link href="/about" className="link block px-4 py-2 text-gray-700 bg-[#1f8cad] rounded-md">WHO WE ARE</Link>
                                             <Link href="/wedo" className="link block px-4 py-2 text-gray-700">WHAT WE DO</Link>
--- app/germany/page.jsx
+++ app/germany/page.jsx
@@ -67,13 +67,17 @@ export default function HomePage() {
             <div className="fullContainer bannerd" id="homeSection">
                 <header>
                     <div className="container">
-                        <div className="logo">
+                    <Link href="/">
+                        <a className="logo">
                             <Image 
-                                src={logoImage} 
-                                height={100} 
-                                alt="Foundation Logo" 
-                                className="rounded-md"/>
-                        </div>
+                            src={logoImage} 
+                            height={100} 
+                            width={100} // Add width to prevent layout shift
+                            alt="Foundation Logo" 
+                            className="rounded-md"
+                            />
+                        </a>
+                        </Link>
 
                         <nav className="pt-5 relative">
                             <div className="flex items-center justify-between px-4 py-3 md:flex md:space-x-4">
@@ -91,7 +95,7 @@ export default function HomePage() {
                                 </div>
                                 <ul className={`mt-4 md:mt-0 md:flex md:space-x-4 ${isOpen ? 'block' : 'hidden'}`}>
                                     <li className="dropdown relative">
-                                        <a href="/" className="dropbtn link ">ABOUT</a>
+                                        <a href="#" className="dropbtn link ">ABOUT</a>
                                         <div className="dropdown-content absolute hidden bg-white shadow-lg rounded-md">
                                             <Link href="/about" className="link block px-4 py-2 text-gray-700">WHO WE ARE</Link>
                                             <Link href="/wedo" className="link block px-4 py-2 text-gray-700">WHAT WE DO</Link>
